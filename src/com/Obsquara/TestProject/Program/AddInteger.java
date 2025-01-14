ebc420e67751927c807431e0d8e914a55cd3dc9c
treas <treas@Stevu>
2025-01-14 21:22:33 +0530

    Testproject commit

---
+++ src/com/Obsquara/TestProject/Program/AddInteger.java
@@ -0,0 +1,16 @@
+package com.Obsquara.TestProject.Program;
+
+public class AddInteger {
+
+	public static void main(String[] args) {
+		{
+			int a =100;
+			int b = 200;
+			int c = a+b;
+			System.out.println("Sum : " +c);
+			}
+
+
+	}
+
+}
